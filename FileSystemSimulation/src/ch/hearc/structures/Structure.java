050153fe2da784c18e8c59251d4d1dd18608e92e
Thomas Lovis <thomas.lovis@gmail.com>
2018-03-06 12:57:13 +0100

    calculate coord string width

--- FileSystemSimulation/src/ch/hearc/structures/Structure.java
+++ FileSystemSimulation/src/ch/hearc/structures/Structure.java
@@ -1,22 +1,46 @@
 package ch.hearc.structures;
 
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
+import ch.hearc.Coord;
+import ch.hearc.Settings;
 import ch.hearc.interfaces.IObjectPaint;
 
 public class Structure implements IObjectPaint {
 
+	private final int DEFAULT_HEIGHT = 40;
+	
 	private ArrayList<StructureSector> structureSectors;
+	private int initX, initY;
 	
 	public Structure(int _numberSector, String[] _structureSectors) {
+		// Default initX and initY = 5
+		this(_numberSector, _structureSectors, 5, 5);
+	}
+	
+	public Structure(int _numberSector, String[] _structureSectors, int _initX, int _initY) {
+		this.initX = _initX;
+		this.initY = _initY;
 		this.structureSectors = new ArrayList<StructureSector>(_numberSector);
 		this.createStructureSector(_structureSectors);
 	}
 	
 	public void createStructureSector(String[] _structureSectors) {
-		for(String _name : _structureSectors) {
-			this.structureSectors.add(new StructureSector(_name));
+		int x = initX, y = initY;
+		for(String name : _structureSectors) {
+			//Calculate string width and height
+			Rectangle2D rect = Settings.DEFAULT_FONT.getStringBounds(name, new FontRenderContext(new AffineTransform(), false, true));
+			int strWidth = (int) rect.getWidth();
+			int strHeight = (int) rect.getHeight();
+			Coord coord = new Coord(x, y, strWidth, DEFAULT_HEIGHT);
+			this.structureSectors.add(new StructureSector(name, coord));
+			x += strWidth;
 		}
 	}
 	
@@ -36,9 +60,13 @@ public class Structure implements IObjectPaint {
 	}
 	
 	public void paint(Graphics _g) {
-		_g.drawRect(0, 0, 500, 40);
+		_g.drawRect(initX, initY, 500, 40);
 		for(StructureSector sector : structureSectors) {
 			sector.paint(_g);
 		}
+	}
+	
+	public void paint(Graphics _g, int _x, int _y) {
+		return;
 	}
 }
