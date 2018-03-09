4f85fffacfdec747ba403a2f98c13ce642194e29
Thomas Lovis <thomas.lovis@gmail.com>
2018-03-09 10:25:03 +0100

    add defaultPanel

---
+++ FileSystemSimulation/src/ch/hearc/panels/DefaultPanel.java
@@ -0,0 +1,14 @@
+package ch.hearc.panels;
+
+import javax.swing.JPanel;
+
+import ch.hearc.Settings;
+
+public class DefaultPanel extends JPanel {
+
+	private static final long serialVersionUID = -6153327367838181922L;
+
+	public DefaultPanel() {
+		this.setBackground(Settings.PANEL_DEFAULT_BACKGROUND_COLOR);
+	}
+}
